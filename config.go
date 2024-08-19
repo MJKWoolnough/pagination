b843b553302c1a7a4049aeb2e14367fae752215c
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-19 17:46:49 +0100

    delint'd

--- config.go
+++ config.go
@@ -30,9 +30,9 @@ func (c Config) Get(currPage, lastPage uint) Pagination {
 	for page := uint(0); page <= lastPage; page++ {
 		if !(page < c.Ends || // Beginning
 			page > lastPage-c.Ends || // End
-			((c.Surrounding > currPage || page >= currPage-c.Surrounding) && page <= currPage+c.Surrounding) || // Middle
-			c.Ends > 0 && ((currPage-c.Surrounding-1 == c.Ends && page == c.Ends) || // Merge Beginning and Middle if close enough
-				(currPage+c.Surrounding+1 == lastPage-c.Ends && page == lastPage-c.Ends))) { // Merge Middle and End if close enough
+			((c.Surrounding > currPage || page >= currPage-c.Surrounding) && page <= currPage+c.Surrounding) || // Middle.
+			c.Ends > 0 && ((currPage-c.Surrounding-1 == c.Ends && page == c.Ends) || // Merge Beginning and Middle if close enough.
+				(currPage+c.Surrounding+1 == lastPage-c.Ends && page == lastPage-c.Ends))) { // Merge Middle and End if close enough.
 
 			if page != start {
 				p.sections[p.numSections] = section{start, page - 1}
